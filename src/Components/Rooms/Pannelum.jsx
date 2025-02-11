70f020d534916865522375706edf78606e94d5e5
Ankit Kumar <ankitkumar003@corp.bharatpetroleum.com>
2025-02-11 12:23:38 +0530

    first commit

--- src/Components/Rooms/Pannelum.jsx
+++ src/Components/Rooms/Pannelum.jsx
@@ -1,46 +1,46 @@
 // import React, { Component } from 'react';
 // import { render } from 'react-dom';
  
-import { Pannellum} from "pannellum-react";
+// import { Pannellum} from "pannellum-react";
 
-import Pano1 from "../../Pano.jpg"
+// import Pano1 from "../../Pano.jpg"
  
  
-const PannellumReact = () => (
-  <div>
+// const PannellumReact = () => (
+//   <div>
 
-    <Pannellum
-        width="100%"
-        height="500px"
-        image={Pano1}
-        pitch={10}
-        yaw={180}
-        hfov={110}
-        autoLoad
-        onLoad={() => {
-            console.log("panorama loaded");
-        }}
-    >
-      {/* <Pannellum.Hotspot
-        type="info"
-        pitch={11}
-        yaw={-167}
-        text="Info Hotspot Text 3"
-        // URL="https://github.com/farminf/pannellum-react"
-      /> */}
+//     <Pannellum
+//         width="100%"
+//         height="500px"
+//         image={Pano1}
+//         pitch={10}
+//         yaw={180}
+//         hfov={110}
+//         autoLoad
+//         onLoad={() => {
+//             console.log("panorama loaded");
+//         }}
+//     >
+//       {/* <Pannellum.Hotspot
+//         type="info"
+//         pitch={11}
+//         yaw={-167}
+//         text="Info Hotspot Text 3"
+//         // URL="https://github.com/farminf/pannellum-react"
+//       /> */}
  
-      <Pannellum.Hotspot
-        type="info"
-        pitch={31}
-        yaw={-107}
-        text="Info Hotspot Text 4"
-        // URL="https://github.com/farminf/pannellum-react"
-      />
-    </Pannellum>
+//       <Pannellum.Hotspot
+//         type="info"
+//         pitch={31}
+//         yaw={-107}
+//         text="Info Hotspot Text 4"
+//         // URL="https://github.com/farminf/pannellum-react"
+//       />
+//     </Pannellum>
  
   
  
-    </div>
-);
+//     </div>
+// );
  
-export default PannellumReact;
+// export default PannellumReact;
